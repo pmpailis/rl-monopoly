9036fe04ceacc5c4c26c2cbe1fb9bd873a03600f
pmpailis <pmpailis@gmail.com>
2020-12-20 10:58:13 +0200

    updating

---
+++ game_engine/monopoly/game_cards.py
@@ -0,0 +1,18 @@
+class GameCard(object):
+
+    def __init__(self):
+        pass
+
+
+class CommunityChestCard(GameCard):
+
+    def __init__(self):
+        super().__init__()
+        pass
+
+
+class ChanceCard(GameCard):
+
+    def __init__(self):
+        super().__init__()
+        pass
---
+++ game_engine/monopoly/player.py
@@ -0,0 +1,5 @@
+class Player(object):
+
+    def __init__(self):
+        pass
+
